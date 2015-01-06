ae6334993c2d3fdd36158f4175802fed7fef3b5f
adam <ajieme@gmail.com>
2015-01-05 18:21:55 -0600

    updated liked items query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.where(username: params[:username]).last
-    @liked_items = ListItem.joins("JOIN likes ON likes.likable_id = list_items.id")
+    @liked_items = ListItem.joins("JOIN likes ON likes.likable_id = list_items.id").last(3)
     @lists = Array.new
     @user.list_items.each do |list_item|  
       unless list_item.list.in? @lists   
