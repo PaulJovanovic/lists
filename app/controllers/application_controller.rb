4595be000543c022c23da17b06a7bdf1b2734d21
PaulJovanovic <paulrealsane@gmail.com>
2015-01-10 16:31:01 -0800

    KO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_access_control_headers
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :set_access_control_headers
 
   private
 
