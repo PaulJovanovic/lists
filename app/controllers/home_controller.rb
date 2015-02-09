4c648e6820af518740dab9da00b4e9facfa773cf
PaulJovanovic <paulrealsane@gmail.com>
2015-02-09 13:06:19 -0800

    Hide lists with less than 3 lists on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.includes(:lists).where("lists_count > ?", 1).order(lists_count: :desc)
+    @categories = Category.includes(:lists).where("lists_count > ?", 2).order(lists_count: :desc)
   end
 end
